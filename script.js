9a7f8220a670a7830bfe5e013dd820cc9baf6b0f
dev793 <117709900+dev793@users.noreply.github.com>
2026-01-22 12:23:05 +1300

    Completed step 2

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+const myLibrary = [];
+
+function Book(id, title, author, pages, read) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function() {
+    return `${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? "has read" : "not read yet"}`;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    const book = new Book(crypto.randomUUID(), title, author, pages, read);
+    myLibrary.push(book);
+}
+
+console.log(myLibrary);
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, true);
+console.log(myLibrary);
